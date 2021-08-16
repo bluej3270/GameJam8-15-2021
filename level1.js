75b8e456c1b230aa0f5f5845fd4201c9e629e3d4
bluej3270 <kellanclapp@gmail.com>
2021-08-15 21:53:11 -0600

    Set up level one class

---
+++ level1.js
@@ -0,0 +1,20 @@
+class level1 extends Phaser.Scene {
+    constructor () {
+        super ('boot');
+    }
+    
+    //Preload (assets)
+    preload() {
+
+    }
+
+    //cerate (runs as the scene loads)
+    create() {
+
+    }
+
+    //runs each frame
+    update() {
+
+    }
+}
